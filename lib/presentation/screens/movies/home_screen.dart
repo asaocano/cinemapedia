f32ce3f48d60d4bc71804e4a1f586f838597dfd2
Asahel Ocaño <57965460+asaocano@users.noreply.github.com>
2026-02-08 15:29:50 -0700

    Se agregan variables de entorno

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cinemapedia/config/constants/environment.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,7 +8,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Placeholder(),
+      body: Center(
+        child: Text(Environment.movieDbKey),
+      ),
     );
   }
 }
